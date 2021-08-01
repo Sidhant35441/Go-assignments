05318edc0c93f9e5a2c25d5deddbab7798323dd1
Sidhant35441 <sidhant.jena60@gmail.com>
2021-08-01 18:28:50 +0530

    remove session-05

--- session-05/controller/custcontroller.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-import "assignments/session-05/domain"
-
-type CustomerController struct {
-	store domain.CustomerStore
-}
--- session-05/go.mod
+++
@@ -1,3 +0,0 @@
-module assignments/session-05
-
-go 1.16
